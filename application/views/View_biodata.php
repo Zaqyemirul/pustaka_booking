c285b8c241e548b6886cbcf971d52e343bcabd99
Zaqyemirul <zaqy.emirul0011@gmail.com>
2023-09-26 15:48:04 +0700

    Tugas Biodata Pertemuan2

---
+++ application/views/View_biodata.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Biodata</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body class="p-3 mb-2 bg-primary text-white">
+        <h2>Biodata Pribadi</h2>
+        <div class="col">
+            <table>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td>Zaqy Emirul Kamil</td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td>10.3A.07</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>:</td>
+                <td>RPL</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>Rawamangun</td>
+            </tr>
+            <tr>
+                <td>No.Hp</td>
+                <td>:</td>
+                <td>085784931086</td>
+            </tr>
+            </table>
+        </div>
+    </body>
+</html>
