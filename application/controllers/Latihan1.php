8f41bf5612cd108e50c9127c268097be2a074abf
Zaqyemirul <zaqy.emirul0011@gmail.com>
2023-10-03 15:58:18 +0700

    Pertemuan 3

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,13 @@
+<?php
+class Latihan1 extends CI_Controller {
+	public function index()
+	{
+		echo "Selamat Datang.. Selamat belajar web Programming";
+	}
+    public function penjumlahan($n1,$n2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1,$n2);
+        echo "Hasil".$hasil;
+    }
+}
